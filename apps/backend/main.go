a35bb7c9857c69555fd180e3b13a3be749cd0c59
AtillaTahak <atillatahaa@gmail.com>
2025-06-13 01:58:04 +0300

    Add Swagger documentation and setup for ByFood Book API

--- apps/backend/main.go
+++ apps/backend/main.go
@@ -2,13 +2,24 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/swaggo/fiber-swagger"
+	_ "github.com/AtillaTahaK/gobooklibrary/docs" // Yerel modül adını kullanın
+
 )
 
+// @title           ByFood Book API
+// @version         1.0
+// @description     API for managing a book library and URL cleaner service
+// @host            localhost:8080
+// @BasePath        /
 func main() {
 	app := fiber.New()
 
+	// Swagger endpoint
+	app.Get("/swagger/*", fiberSwagger.WrapHandler)
+
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+		return c.SendString("Backend working")
 	})
 
 	app.Listen(":8080")
